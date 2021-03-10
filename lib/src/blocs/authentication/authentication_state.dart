ec3e0168b5eb406eb4f158342e25779025801d93
hiep <hiep.developer51@gmail.com>
2021-03-10 23:23:21 +0700

    Reformat code

--- lib/src/blocs/authentication/authentication_state.dart
+++ lib/src/blocs/authentication/authentication_state.dart
@@ -1,20 +1,22 @@
 part of 'authentication_bloc.dart';
 
 class AuthenticationState extends Equatable {
-  const AuthenticationState._(
-      {this.user = User.empty,
-      this.token = '',
-      this.status: AuthenticationStatus.unknown});
+  const AuthenticationState._({
+    this.user = User.empty,
+    this.token = '',
+    this.status: AuthenticationStatus.unknown,
+  });
 
   const AuthenticationState.unknown() : this._();
 
   const AuthenticationState.authenticated(
       {@required User user, @required String token})
       : this._(
-            // status: AuthenticationStatus.authenticated,
-            user: user,
-            token: token,
-            status: AuthenticationStatus.authenticated);
+          // status: AuthenticationStatus.authenticated,
+          user: user,
+          token: token,
+          status: AuthenticationStatus.authenticated,
+        );
 
   const AuthenticationState.unauthenticated()
       : this._(token: '', status: AuthenticationStatus.unauthenticated);
